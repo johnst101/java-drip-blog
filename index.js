525fa6c1a73dd24eb57315f56f12c8106739f9bd
johnst101 <tylercjohnson16@gmail.com>
2022-08-06 14:55:45 -0500

    structure and style finished; working on function

---
+++ index.js
@@ -0,0 +1,16 @@
+// template_epai36s
+// service_ununibc
+// bhCwExiy77joHbRXY
+/* <script type="text/javascript"
+src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("YOUR_PUBLIC_KEY");
+   })();
+</script> */
+
+function contact() {
+  event.preventDefault();
+  console.log("It worked")
+}
